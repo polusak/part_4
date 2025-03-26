733310a9f8477f9d6de104f21cead0303d32009a
Aku <ap@Aku-MacBook-Air.local>
2025-03-26 08:09:44 +0200

    4.8

--- blogilista/index.js
+++ blogilista/index.js
@@ -1,32 +1,9 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const { info, error } = require('./utils/logger')
+const app = require('./app')
 const config = require('./utils/config')
-const middleware = require('./utils/middleware')
-const blogsRouter = require('./controllers/blogs')
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
-  .then(result => {
-    if (result) {
-      info('connected to MongoDB')
-    }
-  })
-  .catch((err) => {
-    error('error connecting to MongoDB:', err.message)
-  })
-
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
-app.use('/api/blogs', blogsRouter)
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+const logger = require('./utils/logger')
 
 const PORT = config.PORT
 app.listen(PORT, () => {
-  info(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
 
