5c086855ebc9edda501a6511176b4d09daa53012
Aku <ap@Aku-MacBook-Air.local>
2025-03-26 08:47:39 +0200

    4.9

--- blogilista/tests/blog_api.test.js
+++ blogilista/tests/blog_api.test.js
@@ -1,15 +1,24 @@
 const { test, after } = require('node:test')
+const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
 
-test('notes are returned as json', async () => {
+test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('blogs are identified with field `id` not `_id`', async () => {
+  const response = await api.get('/api/blogs')
+  const listOfBlogs = response.body
+  listOfBlogs.forEach(blog => {
+    assert(blog.id && !blog._id)
+  })
 })
 
 after(async () => {
