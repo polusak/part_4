caadbfef640a0d1d74d0e164b9eaef94a73b50e4
Aku <ap@Aku-MacBook-Air.local>
2025-03-28 13:31:30 +0200

    4.18

--- blogilista/controllers/blogs.js
+++ blogilista/controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', (request, response) => {
   Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
@@ -24,8 +33,12 @@ blogsRouter.get('/:id', (request, response, next) => {
 
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
-  const users = await User.find({})
-  const user = users[0]
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
